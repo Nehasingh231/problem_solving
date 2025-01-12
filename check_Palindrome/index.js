4fc14e73766fb20ef19d6fbc4583bf20abae9cfd
Nehasingh231 <nehakumari14072001@gmail.com>
2025-01-12 15:26:47 +0530

    palindrome check

--- check_Palindrome/index.js
+++ check_Palindrome/index.js
@@ -1,1 +1,28 @@
-co
+const checkPalindrome  = (string) => {
+    const checkFirst = string.toLowerCase().split(" ").join("");
+    const checkAnother = checkFirst.split("").reverse().join("");
+
+    return checkFirst === checkAnother;
+}
+
+const result = checkPalindrome("madam");
+console.log(result);
+console.log(checkPalindrome("a man a plan a canal panama"));
+console.log(checkPalindrome("ma dam"))
+console.log(checkPalindrome("hello"))
+
+
+
+function isPalindrome (num) {
+    const number = num.toString();
+    const reverse = number.split("").reverse().join("");
+
+    return number === reverse;
+}
+
+console.log(isPalindrome("258"))
+console.log(isPalindrome("121"));
+
+
+
+
